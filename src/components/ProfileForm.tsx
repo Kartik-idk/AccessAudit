d571627f53495b53d2b514fb502c18b97913e26a
Kartikeya Pradhan <kartikeyapradhan@Kartikeyas-MacBook-Air.local>
2026-09-20 16:18:25 +0530

    Test fixtures

--- src/components/ProfileForm.tsx
+++ src/components/ProfileForm.tsx
@@ -36,6 +36,30 @@ export function ProfileForm() {
 
       <Avatar profile={profile} />
 
+      {/* Critical Test Cases */}
+      {/* Case 1 */}
+      <img src="/case1.png" />
+      
+      {/* Case 2 */}
+      <img
+        src="/case2.png"
+      />
+
+      {/* Case 3 */}
+      <img
+        src="/case3.png"
+        className="avatar"
+      />
+
+      {/* Case 4 */}
+      <img
+        className="avatar"
+        src="/case4.png"
+      />
+
+      {/* Multiple elements on one line */}
+      <div className="gallery"><img src="/multi1.png" /><img src="/multi2.png" /></div>
+
       <button type="submit">
         Save
       </button>
